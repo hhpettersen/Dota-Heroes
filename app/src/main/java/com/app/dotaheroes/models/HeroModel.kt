d91fa58624e3f2ad9c7e94fbe65e501abfbbec3a
hhpettersen <helg.pett@gmail.com>
2021-04-11 09:09:49 +0200

    model file for dota heroes

---
+++ app/src/main/java/com/app/dotaheroes/models/HeroModel.kt
@@ -0,0 +1,9 @@
+package com.app.dotaheroes.models
+
+data class HeroStats(
+    val id: Int? = 0,
+    val name: String? = "",
+    val localized_name: String? = "",
+    val img: String? = "",
+    val icon: String? = ""
+)
