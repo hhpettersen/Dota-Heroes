93f1d7c04821943c1f3ecebf740e2967da5690fe
hhpettersen <helg.pett@gmail.com>
2021-04-11 10:04:31 +0200

    Removed unused url

--- app/src/main/java/com/app/dotaheroes/other/Constants.kt
+++ app/src/main/java/com/app/dotaheroes/other/Constants.kt
@@ -2,5 +2,4 @@ package com.app.dotaheroes.other
 
 object Constants {
     const val BASE_URL = "https://api.opendota.com/api/"
-    const val DOTA_URL = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key?key=%3Ckey%3E"
 }
